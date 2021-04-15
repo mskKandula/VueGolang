68b5e401ac6467e47eefefbf261fb6ea920d24df
mskKandula <mskKandula@gmail.com>
2021-04-15 11:01:29 +0530

    Created LineChart in Vuejs

---
+++ RealtimeDashboard/Client/dashboard/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Line</router-link> |
+      <router-link to="/bar">Bar</router-link> |
+      <router-link to="/doughnut">Doughnut</router-link> |
+      <router-link to="/pie">Pie</router-link> |
+      <router-link to="/radar">Radar</router-link> |
+      <router-link to="/polar-area">Polar Area</router-link> |
+      <router-link to="/bubble">Bubble</router-link> |
+      <router-link to="/scatter">Scatter</router-link>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <router-view />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style >
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px 30px 60px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
---
+++ RealtimeDashboard/Client/dashboard/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Line Chart Example in Vue</h3>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart'
+
+export default {
+  components: {
+    LineChart
+  }
+}
+</script>
