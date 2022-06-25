42644e0fb1c2730c29c68ab4c8fcb40ec3d47e30
mohanak <mohanak@mkcl.org>
2022-06-25 18:18:18 +0530

    Create Room

---
+++ VideoChatApplication/Client/video-chat/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <button @click="createRoom">Create Room</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    async createRoom() {
+      const resp = await fetch("http://localhost:8000/create");
+      console.log(resp);
+      const { roomId } = await resp.json();
+      console.log("15", roomId);
+      this.$router.push(`/room/${roomId}`);
+    },
+  },
+};
+</script>
