ea9773e64abf6c84a1ed10708d5e6d7018eb45f4
mohanak <mohanak@mkcl.org>
2022-07-13 10:40:42 +0530

    Screen Recording Initialization

--- screen-capture/src/App.vue
+++ screen-capture/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ScreenRecoding />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScreenRecoding from './components/ScreenRecoding.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScreenRecoding
   }
 }
 </script>
---
+++ screen-capture/src/components/ScreenRecoding.vue
@@ -0,0 +1,38 @@
+<template>
+   <div id="app">
+    <div class="mt-5 mb-5">
+        <div v-if="!isRecording">
+      <button
+        @click="getStream"
+        v-show="canRecord"
+        class="ml-10"
+      >
+        Start Recording 🎥</button
+      >
+</div>
+      <div v-else>
+        <button @click="stopStream"> Stop Screen Recording ❌ </button>
+      </div>
+  </div>
+   </div>
+</template>
+
+<script>
+export default {
+ data() {
+    return {
+      isRecording:false,
+      canRecord: true,
+    }
+ },
+  methods: {
+    getStream() {
+    },
+    stopStream(){}
+  }
+}
+</script>
+
+<style>
+
+</style>
