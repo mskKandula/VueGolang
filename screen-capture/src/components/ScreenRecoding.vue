ea2c4a82137fcdf51278f27ee6903f7b08196643
mohanak <mohanak@mkcl.org>
2022-07-15 11:35:32 +0530

    Video Download

--- screen-capture/src/components/ScreenRecoding.vue
+++ screen-capture/src/components/ScreenRecoding.vue
@@ -24,7 +24,7 @@ export default {
     return {
       isRecording:false,
       canRecord: true,
-      videoTrack: null,
+      // videoTrack: null,
       displayOptions: {
         video: {
           cursor: "always",
@@ -34,7 +34,8 @@ export default {
           noiseSuppression: true,
           sampleRate: 44100,
         },
-      }
+      },
+      videoData: []
     }
  },
   methods: {
@@ -45,10 +46,45 @@ export default {
           let video = document.getElementById("video");
           video.srcObject = stream;
          
-          this.videoTrack = stream.getVideoTracks()[0];
+          // this.videoTrack = stream.getVideoTracks()[0];
+
+          let mediaRecorder = new MediaRecorder(stream);
+
+          mediaRecorder.ondataavailable = function (e) {
+                    console.log({e})
+                    this.videoData.push(e.data)
+                }
+
+                mediaRecorder.onstop = () => {
+                  this.downloadVideo()
+ stream.getTracks().forEach(function (track) {
+                        track.stop();
+                    });
+                }
         })
 
         .catch((e) => console.log(e));
+    },
+    downloadVideo(){
+    
+                   
+    var blob = new Blob(this.videoData, { type: 'video/webm' });
+    
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.webm';
+    document.body.appendChild(a);
+    a.click();
+    this.clearData()
+       
+
+    },
+    clearData(){
+      this.videoData=[]
+ document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
     },
     stopStream(){}
   }
