4b6001b5d5efea7be322740783a6166dced9b146
mohanak <mohanak@mkcl.org>
2022-08-05 09:57:26 +0530

    Project Initialization

---
+++ LiveStream/live-stream/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <LiveStreaming />
+  </div>
+</template>
+
+<script>
+import LiveStreaming from "./components/LiveStreaming.vue";
+
+export default {
+  name: "App",
+  components: {
+    LiveStreaming,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
