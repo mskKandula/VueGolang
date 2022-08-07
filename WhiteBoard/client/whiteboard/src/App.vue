18218f245bd49a87ea233007fd120da3c9095b9a
mohanak <mohanak@mkcl.org>
2022-08-07 16:28:40 +0530

    WhiteBorad Project Initialization

---
+++ WhiteBoard/client/whiteboard/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   
+    <Container />
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
