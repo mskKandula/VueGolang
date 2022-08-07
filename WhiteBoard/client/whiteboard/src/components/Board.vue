094eeaa19ae1b4617821d4c0cd9812474166f885
mohanak <mohanak@mkcl.org>
2022-08-07 17:01:47 +0530

    On Paint

--- WhiteBoard/client/whiteboard/src/components/Board.vue
+++ WhiteBoard/client/whiteboard/src/components/Board.vue
@@ -13,30 +13,34 @@ export default {
   data() {
     return {
       ctx: null,
+      canvas: null,
+      mouse: null,
+      last_mouse:null,
+      timeout:null
     };
   },
   methods: {
     drawOnCanvas() {
-      var canvas = document.querySelector("#board");
-      this.ctx = canvas.getContext("2d");
+       this.canvas = document.querySelector("#board");
+      this.ctx = this.canvas.getContext("2d");
 
       var sketch = document.querySelector("#sketch");
       var sketch_style = getComputedStyle(sketch);
-      canvas.width = parseInt(sketch_style.getPropertyValue("width"));
-      canvas.height = parseInt(sketch_style.getPropertyValue("height"));
+      this.canvas.width = parseInt(sketch_style.getPropertyValue("width"));
+      this.canvas.height = parseInt(sketch_style.getPropertyValue("height"));
 
-      var mouse = { x: 0, y: 0 };
-      var last_mouse = { x: 0, y: 0 };
+       this.mouse = { x: 0, y: 0 };
+       this.last_mouse = { x: 0, y: 0 };
 
       /* Mouse Capturing Work */
-      canvas.addEventListener(
+      this.canvas.addEventListener(
         "mousemove",
         function (e) {
-          last_mouse.x = mouse.x;
-          last_mouse.y = mouse.y;
+          this.last_mouse.x = this.mouse.x;
+          this.last_mouse.y = this.mouse.y;
 
-          mouse.x = e.pageX - this.offsetLeft;
-          mouse.y = e.pageY - this.offsetTop;
+          this.mouse.x = e.pageX - this.offsetLeft;
+          this.mouse.y = e.pageY - this.offsetTop;
         },
         false
       );
@@ -47,28 +51,49 @@ export default {
       this.ctx.lineCap = "round";
       this.ctx.strokeStyle = this.color;
 
-      canvas.addEventListener(
+      this.canvas.addEventListener(
         "mousedown",
         function () {
-          canvas.addEventListener("mousemove", this.onPaint, false);
+          this.canvas.addEventListener("mousemove", this.onPaint, false);
         },
         false
       );
 
-      canvas.addEventListener(
+      this.canvas.addEventListener(
         "mouseup",
         function () {
-          canvas.removeEventListener("mousemove", this.onPaint, false);
+          this.canvas.removeEventListener("mousemove", this.onPaint, false);
         },
         false
       );
     },
     onPaint(){
+ this.ctx.beginPath();
+            this.ctx.moveTo(this.last_mouse.x, this.last_mouse.y);
+            this.ctx.lineTo(this.mouse.x, this.mouse.y);
+            this.ctx.closePath();
+            this.ctx.stroke();
 
-    }
+            if(this.timeout != undefined){ 
+                clearTimeout(this.timeout)}
+
+            this.timeout = setTimeout(function(){
+                let base64ImageData = this.canvas.toDataURL("image/png");
+               console.log(base64ImageData)
+            })
   },
+  }
 };
 </script>
 
 <style>
+.board {
+  width: 100%;
+  height: 100%;
+}
+
+.sketch {
+  width: 100%;
+  height: 100%;
+}
 </style>
